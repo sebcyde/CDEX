c5d5d159cfa7f1ed8741e234f225017653d11658
Seb <sebcyde@hotmail.com>
2022-07-04 21:16:25 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { Navbar, Icon, NavItem } from 'react-materialize';
+import M from 'materialize-css';
+import Dashboard from './Components/Dashboard/Dashboard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Navbar
+					alignLinks="left"
+					brand={
+						<a className="brand-logo right NavTitle" href="#">
+							CDEX
+						</a>
+					}
+					id="mobile-nav"
+					menuIcon={<Icon>menu</Icon>}
+					options={{
+						draggable: true,
+						edge: 'left',
+						inDuration: 250,
+						onCloseEnd: null,
+						onCloseStart: null,
+						onOpenEnd: null,
+						onOpenStart: null,
+						outDuration: 200,
+						preventScrolling: true,
+					}}
+				>
+					<NavItem onClick={function noRefCheck() {}}>Getting started</NavItem>
+					<NavItem href="components.html">Components</NavItem>
+				</Navbar>
+				<Routes>
+					<Route exact path="/" element={<Dashboard />} />
+					{/* <Route path="/contact" component={} />
+					<Route path="/about" component={} /> */}
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
